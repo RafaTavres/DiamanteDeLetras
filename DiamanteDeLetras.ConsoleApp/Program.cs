ec0417de9995751aa2e61758417aab1be23490f0
Rafael S Tavares <ragbriros@gmail.com>
2023-03-22 14:40:11 -0300

    Melhora no Código

--- DiamanteDeLetras.ConsoleApp/Program.cs
+++ DiamanteDeLetras.ConsoleApp/Program.cs
@@ -1,91 +1,117 @@
 ﻿using System;
+using System.Drawing;
 
 namespace DiamanteDeLetras.ConsoleApp
 {
     internal class Program
     {
+        static int linhaIncial = 1;
+        static int letraEmInt = 65;
+        static int tamanhoFinal;
+        static bool eNumero;
         static void Main(string[] args)
         {
             const string SAIR = "S";
             string respostaSAIR = "";
-
             while (respostaSAIR.ToUpper() != SAIR)
             {
-                int linhaIncial = 1;
-                int letraEmInt = 65;
-                Console.WriteLine("\n------------------");
-
-                Console.WriteLine("Até Qual Letra vai o Diamante: ");
-                char tamanhoEmChar = Convert.ToChar(Console.ReadLine().ToUpper());
-                int tamanhoFinal = Convert.ToInt32(tamanhoEmChar) - 65;
-                string charParaString = Convert.ToString(tamanhoEmChar);
-                bool eNumero = charParaString.All(char.IsDigit);
+                Menu();
 
                 if (eNumero)
-                {
-                    Console.WriteLine("Erro: O valor deve ser letra...");
-                    continue;
-                }
-
-                #region Pra Cima
-                for (int i = 1; tamanhoFinal > 0; i++)
-                    {
-
-                        for (int j = 1; j <= tamanhoFinal; j++)
-                        {
-                            Console.Write(" ");
-                        }
-
-                        for (int j = 1; j <= linhaIncial; j++)
-                        {
-                            if (j == 1 || j == linhaIncial)
-                            {
-                                Console.Write(Convert.ToChar(letraEmInt));
-                            }
-                            Console.Write(" ");
-
-                        }
-
-                        tamanhoFinal--;
-                        linhaIncial += 2;
-                        letraEmInt += 1;
-                        Console.WriteLine("");
-                        Console.WriteLine("");
-
-                }
-                #endregion
-
-                #region Pra Baixo
-                for (int i = 1; linhaIncial > 0; i++)
-                {
-
-                    for (int j = 1; j <= tamanhoFinal; j++)
-                    {
-                        Console.Write(" ");
-                    }
-
-                    for (int j = 1; j <= linhaIncial; j++)
-                    {
-                        if (j == 1 || j == linhaIncial)
-                        {
-                            Console.Write(Convert.ToChar(letraEmInt));
-                        }
-                        Console.Write(" ");
-                    }
-
-                    tamanhoFinal++;
-                    linhaIncial -= 2;
-                    letraEmInt -= 1;
-                    Console.WriteLine("");
-                    Console.WriteLine("");
-                }
-                #endregion
+                    MensagemDeErro("variavel nao é número...");
+                 
+                PraCima();              
+                PraBaixo();
+                ResetaValor();
 
                 Console.WriteLine("Sair S/N");
                 respostaSAIR = Console.ReadLine();
 
             }
             Console.ReadLine();
+
+        }
+
+        private static void PraBaixo()
+        {
+            for (int i = 0; linhaIncial > 0; i++)
+            {
+
+                for (int j = 1; j <= tamanhoFinal; j++)
+                {
+
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= linhaIncial; j++)
+                {
+                    if (j == 1 || j == linhaIncial)
+                    {
+                        Console.Write(Convert.ToChar(letraEmInt));
+                    }
+                    Console.Write(" ");
+
+                }
+
+                tamanhoFinal++;
+                linhaIncial -= 2;
+                letraEmInt -= 1;
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+        }
+
+        private static void PraCima()
+        {
+            for (int i = 1; tamanhoFinal > 0; i++)
+            {
+
+                for (int j = 1; j <= tamanhoFinal; j++)
+                {
+  
+                    Console.Write(" ");
+
+                }
+
+                for (int j = 1; j <= linhaIncial; j++)
+                {
+                    if (j == 1 || j == linhaIncial)
+                    {
+                        Console.Write(Convert.ToChar(letraEmInt));
+                    }
+                    Console.Write(" ");
+
+                }
+
+                tamanhoFinal--;
+                linhaIncial += 2;
+                letraEmInt += 1;
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+            }
+        }
+
+         static void MensagemDeErro(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(mensagem);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+         static void Menu()
+        {
+            Console.WriteLine("\n------------------");
+            Console.WriteLine("Até Qual Letra vai o Diamante: ");
+            char tamanhoEmChar = Convert.ToChar(Console.ReadLine().ToUpper());
+            tamanhoFinal = Convert.ToInt32(tamanhoEmChar) - 65;
+            string charParaString = Convert.ToString(tamanhoEmChar);
+            eNumero = charParaString.All(char.IsDigit);
+        }
+        static void ResetaValor()
+        {
+             linhaIncial = 1;
+             letraEmInt = 65;
         }
     }
 }
